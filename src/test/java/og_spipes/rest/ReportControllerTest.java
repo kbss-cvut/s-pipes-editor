c5f94a983e0bad685e83b51d49aa9bd70a97bf37
Petr Jordan <petr.jordan@external.t-mobile.cz>
2021-02-05 21:40:49 +0100

    REST-API mockMvc test for Reports Controller

--- src/test/java/og_spipes/rest/ReportControllerTest.java
+++ src/test/java/og_spipes/rest/ReportControllerTest.java
@@ -1,5 +1,26 @@
 package og_spipes.rest;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 class ReportControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testGetReports() throws Exception {
+        //TODO enforce some assertion
+        this.mockMvc.perform(get("/reports"))
+                .andDo(print());
+    }
 
 }
