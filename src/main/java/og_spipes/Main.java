b7feb6ad26d20533c65a27ca895eb9b4eeae567c
Petr Jordan <petr.jordan@external.t-mobile.cz>
2021-04-12 23:03:04 +0200

    FormControllerTest working

--- src/main/java/og_spipes/Main.java
+++ src/main/java/og_spipes/Main.java
@@ -18,9 +18,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Example08 {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Example08.class, args);
+        SpringApplication.run(Main.class, args);
     }
 }
