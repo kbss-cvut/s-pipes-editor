d81ecce8b1b2746990fdc1d45823179dd28444c2
Bogdan Kostov <kostobog@fel.cvut.cz>
2023-08-17 18:45:21 +0200

    [FIX kbss-cvut/s-pipes-editor-ui#24] Fix request parameters in request to s-pipes service.

--- src/main/java/og_spipes/service/SPipesExecutionService.java
+++ src/main/java/og_spipes/service/SPipesExecutionService.java
@@ -61,8 +61,7 @@ public class SPipesExecutionService {
             Map<String, String> params
     ) throws SPipesEngineException {
         String serviceUrl = engineUrl + "/service";
-        params.put("id", functionId);
-        params.put("_pConfigURL", pConfigURL);
+        params.put("_pId", functionId);
 
         UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(serviceUrl);
         for (Map.Entry<String, String> pair : params.entrySet()) {
