3499de784c5f4be886d8e8a6505bf509c699525e
Daniil Palagin <daniilpalaginjob@gmail.com>
2025-02-20 09:37:34 +0100

    [#32] Implement configuration class

--- src/main/java/og_spipes/Main.java
+++ src/main/java/og_spipes/Main.java
@@ -16,8 +16,10 @@ package og_spipes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Main {
 
     public static void main(String[] args) {
